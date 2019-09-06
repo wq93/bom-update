be7995b98e34cce5b113b5fca7a4f88916230ef0
wangqi <wangqi@chicv.com>
2019-09-06 18:57:54 +0800

    chang size toggle table

--- patternNum.js
+++ patternNum.js
@@ -1,17 +1,17 @@
 // 假设基码
 var basicSize = 0;
 var basicSizeCode = '';
-var sizeArr = ['XS', 'S', 'M', 'L', 'XL','XXL','3XL','4XL','5XL','6XL','7XL','8XL','9XL']
+var sizeArr = [ 'XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL', '4XL', '5XL', '6XL', '7XL', '8XL', '9XL' ]
 var initTr = '';
 
 // 设置表头 && 根据头部生成一个空行以便添加
 function renderThead(data) {
   initTr = ''
-  var itemFir = data.item[0];
+  var itemFir = data[ 0 ];
   basicSizeCode = itemFir.size_base;
   var theadStart = `<tr><th>尺码部位</th>
                   <th>测量方法</th>
-                  <th >基码（${itemFir.size_base}）</th>
+                  <th >基码（${ itemFir.size_base }）</th>
                   `
   var theadEnd = `<th>误差 <input type="checkbox" class="sync-mistake">同步</th>
               <th>操作</th></tr>`
@@ -24,22 +24,22 @@ function renderThead(data) {
                 <input type="text" value="" name="" class='w80 metering-type'>
               </td>
               <td>
-                <input type="number" step="0.01" data-size-base=${itemFir.size_base} value="0" name="" class='w60 basic-code'>
+                <input type="number" step="0.01" data-size-base=${ itemFir.size_base } value="0" name="" class='w60 basic-code'>
               </td>`
   var theadMiddle = ''
   var jumpArr = itemFir.cList
   for (var i = 0; i < jumpArr.length; i++) {
-    var jumpItem = jumpArr[i]
+    var jumpItem = jumpArr[ i ]
     // 最后一个不需要跳码列
     if (i === jumpArr.length - 1) {
-      theadMiddle += `<th>${jumpItem.cm}</th>`
-      initTr += ` <td class="${jumpItem.cm === basicSizeCode ? 'basic-size-td' : ''}" data-last-cm=${jumpItem.cm}"></td>`
+      theadMiddle += `<th>${ jumpItem.cm }</th>`
+      initTr += ` <td class="${ jumpItem.cm === basicSizeCode ? 'basic-size-td' : '' }" data-last-cm=${ jumpItem.cm }"></td>`
     } else {
-      theadMiddle += `<th class=${jumpItem.cm}>${jumpItem.cm}</th>
-              <th>跳码 <input type="checkbox" class="sync-code sync-jump-code-${jumpItem.cm}" data-size="${jumpItem.cm}">同步</th>`
-      initTr += ` <td class="${jumpItem.cm === basicSizeCode ? 'basic-size-td' : ''}"></td>
+      theadMiddle += `<th class=${ jumpItem.cm }>${ jumpItem.cm }</th>
+              <th>跳码 <input type="checkbox" class="sync-code sync-jump-code-${ jumpItem.cm }" data-size="${ jumpItem.cm }">同步</th>`
+      initTr += ` <td class="${ jumpItem.cm === basicSizeCode ? 'basic-size-td' : '' }"></td>
               <td>
-                <input type="number" step="0.01" value="" name="" class='w60 jump-code' data-size="${jumpItem.cm}">
+                <input type="number" step="0.01" value="" name="" class='w60 jump-code' data-size="${ jumpItem.cm }">
               </td>`
     }
   }
@@ -55,36 +55,36 @@ function renderThead(data) {
 
 // 设置表单体
 function renderTbody(data) {
-  var trArr = data.item;
+  var trArr = data;
   var tbodyHtml = '';
   for (var i = 0; i < trArr.length; i++) {
-    var trItem = trArr[i];
+    var trItem = trArr[ i ];
     var trItemCList = trItem.cList;
     tbodyHtml += `<tr class='size-item'>
               <td>
-                <input type="text" value=${trItem.part} name="" class='w80 part'>
+                <input type="text" value=${ trItem.part } name="" class='w80 part'>
               </td>
               <td>
-                <input type="text" value=${trItem.metering_type} name="" class='w80 metering-type'>
+                <input type="text" value=${ trItem.metering_type } name="" class='w80 metering-type'>
               </td>
               <td>
-                <input type="number" step="0.01" data-size-base=${trItem.size_base} value=${trItem.size_base_value} name="" class='w60 basic-code'>
+                <input type="number" step="0.01" data-size-base=${ trItem.size_base } value=${ trItem.size_base_value } name="" class='w60 basic-code'>
               </td>`
     // 循环跳码模块
     for (var j = 0; j < trItemCList.length; j++) {
-      var cListItem = trItemCList[j];
+      var cListItem = trItemCList[ j ];
       // 最后一个不需要跳码列
       if (j === trItemCList.length - 1) {
-        tbodyHtml += ` <td class="${cListItem.cm === basicSizeCode ? 'basic-size-td' : ''}" data-last-cm=${cListItem.cm}>${cListItem.value || ''}</td>`
+        tbodyHtml += ` <td class="${ cListItem.cm === basicSizeCode ? 'basic-size-td' : '' }" data-last-cm=${ cListItem.cm }>${ cListItem.value || '' }</td>`
       } else {
-        tbodyHtml += ` <td class="${cListItem.cm === basicSizeCode ? 'basic-size-td' : ''}">${cListItem.value || ''}</td>
+        tbodyHtml += ` <td class="${ cListItem.cm === basicSizeCode ? 'basic-size-td' : '' }">${ cListItem.value || '' }</td>
               <td>
-                <input type="number" step="0.01" value="${cListItem.tm}" name="" class='w60 jump-code' data-index=${i} data-size=${cListItem.cm}>
+                <input type="number" step="0.01" value="${ cListItem.tm }" name="" class='w60 jump-code' data-index=${ i } data-size=${ cListItem.cm }>
               </td>`
       }
     }
     tbodyHtml += `<td>
-                ±<input type="number" step="0.01"value="${trItem.error}" name="" class='w60 mistake-code'>
+                ±<input type="number" step="0.01"value="${ trItem.error }" name="" class='w60 mistake-code'>
               </td>
               <td>
                 <button type="button" class="btn btn-danger delete-size-item">删除</button>
@@ -95,11 +95,16 @@ function renderTbody(data) {
 
 // 渲染勾选框
 function renderSizecheckboxs(data) {
+  var cListFir = data.item[ 0 ];
+  var basicSize = cListFir.size_base;
+
   // 筛出包含的尺码
-  var cmList = $.map(data.item[0].cList, (item) => item.cm);
+  var cmList = $.map(cListFir.cList, (item) => item.cm);
   var strHtml = $.map(sizeArr, function (item) {
+    var checked = cmList.includes(item) ? 'checked' : '';
+    var disabled = item === basicSize ? 'disabled' : '';
     return `<label>
-              <input type="checkbox" data-id=${ item } class='size-checkbox' ${cmList.includes(item)? 'checked' : ''}>${ item }
+              <input type="checkbox" data-cm=${ item } class='size-checkbox' ${ checked } ${ disabled }>${ item }
             </label>`;
   }).join('');
 
@@ -109,7 +114,7 @@ function renderSizecheckboxs(data) {
 // 根据跳码计算尺码
 function computeSize($target) {
   var currentSize = $($target).attr('data-size');
-  if (compareElement({basicSize: basicSizeCode, currentSize: currentSize})) {
+  if (compareElement({ basicSize: basicSizeCode, currentSize: currentSize })) {
     // 获取父节点的上一个兄弟节点
     var targetParent = $target.parent('td').next('td');
     var standard = $target.parent('td').prev('td');
@@ -130,7 +135,7 @@ function formatSubmitData() {
   // 循环列
   for (var i = 0; i < $trs.length; i++) {
     var trItemData = {}
-    var $tr = $($trs[i])
+    var $tr = $($trs[ i ])
     var cListData = []
     var $cList = $tr.find('.jump-code')
     // 获取最后一个尺码列
@@ -138,7 +143,7 @@ function formatSubmitData() {
     // 循环跳码块
     for (var j = 0; j < $cList.length; j++) {
       var cListItemData = {}
-      var $cListItem = $($cList[j]);
+      var $cListItem = $($cList[ j ]);
       cListItemData = {
         'px': j,
         'tm': $cListItem.val(),
@@ -188,7 +193,7 @@ $('#set-all-jump-code').click(function (e) {
   // 获取所有的跳码框并设置尺码
   for (var i = 0; i < $JumpCode.length; i++) {
     // 获取父节点的上一个兄弟节点
-    var $jumpCode = $($JumpCode[i]);
+    var $jumpCode = $($JumpCode[ i ]);
     computeSize($jumpCode)
   }
 })
@@ -203,13 +208,13 @@ $("#size-table").on("change", ".sync-code", function (e) {
   if (isSync) {
     // 获取当前列的跳码框
     var columnJumpCodes = $('.jump-code[data-size=' + sizeType + ']')
-    columnJumpCodes.val($(columnJumpCodes[0]).val())
+    columnJumpCodes.val($(columnJumpCodes[ 0 ]).val())
     // 改变所有的码
     // 获取所有的跳码框并设置尺码
     var $JumpCode = $('#size-table .jump-code');
     for (var i = 0; i < $JumpCode.length; i++) {
       // 获取父节点的上一个兄弟节点
-      var $jumpCode = $($JumpCode[i]);
+      var $jumpCode = $($JumpCode[ i ]);
       computeSize($jumpCode)
     }
   }
@@ -232,17 +237,16 @@ $("#size-table").on("change", ".size-item:first .jump-code", function (e) {
     var $JumpCode = $('#size-table .jump-code');
     for (var i = 0; i < $JumpCode.length; i++) {
       // 获取父节点的上一个兄弟节点
-      var $jumpCode = $($JumpCode[i]);
+      var $jumpCode = $($JumpCode[ i ]);
       computeSize($jumpCode)
     }
-  }
-  else {
+  } else {
     // 改变这一行的码
     // 获取这行所有的跳码框并设置尺码
     var $JumpCode = target.parents('tr.size-item').find('.jump-code');
     for (var i = 0; i < $JumpCode.length; i++) {
       // 获取父节点的上一个兄弟节点
-      var $jumpCode = $($JumpCode[i]);
+      var $jumpCode = $($JumpCode[ i ]);
       computeSize($jumpCode)
     }
   }
@@ -256,7 +260,7 @@ $("#size-table").on("change", ".size-item:not(:first) .jump-code", function (e) 
   var $JumpCode = target.parents('tr.size-item').find('.jump-code');
   for (var i = 0; i < $JumpCode.length; i++) {
     // 获取父节点的上一个兄弟节点
-    var $jumpCode = $($JumpCode[i]);
+    var $jumpCode = $($JumpCode[ i ]);
     computeSize($jumpCode)
   }
 })
@@ -269,21 +273,21 @@ $("#size-table").on("change", ".basic-code", function (e) {
   var jumpCodes = target.parents('tr.size-item').find('.jump-code');
   for (var i = 0; i < jumpCodes.length; i++) {
     // 获取父节点的上一个兄弟节点
-    var $jumpCode = $(jumpCodes[i]);
+    var $jumpCode = $(jumpCodes[ i ]);
     // 改变这行的基码列
     $jumpCode.parents('tr.size-item').find('.basic-size-td').html(target.val())
     computeSize($jumpCode)
   }
 
-  setTimeout(()=>{
+  setTimeout(() => {
     for (var i = 0; i < jumpCodes.length; i++) {
       // 获取父节点的上一个兄弟节点
-      var $jumpCode = $(jumpCodes[i]);
+      var $jumpCode = $(jumpCodes[ i ]);
       // 改变这行的基码列
       $jumpCode.parents('tr.size-item').find('.basic-size-td').html(target.val())
       computeSize($jumpCode)
     }
-  },0)
+  }, 0)
 });
 
 // 添加一行
@@ -303,7 +307,7 @@ $("#size-table").on("change", ".sync-mistake", function (e) {
   // 是否同步
   var isSync = $('.sync-mistake').is(':checked');
   if (isSync) {
-    $('.mistake-code').val($($('.mistake-code')[0]).val());
+    $('.mistake-code').val($($('.mistake-code')[ 0 ]).val());
   }
 })
 
@@ -320,36 +324,48 @@ $("#size-table").on("change", ".mistake-code:first", function (e) {
 // 提交事件
 $("#submit-table-btn").click(function (e) {
   $.ajax({
-    type:"POST",
-    url:'/api/platemakingSizePart/addPlatemakingSizePart',
-    dataType:"json",
+    type: "POST",
+    url: '/api/platemakingSizePart/addPlatemakingSizePart',
+    dataType: "json",
     contentType: "application/json; charset=utf-8",
     data: JSON.stringify(formatSubmitData()),
-    success:function(data){
-      if (data.success==true) {
+    success: function (data) {
+      if (data.success == true) {
 
       } else {
 
       }
     },
-    error:function(jqXHR){
+    error: function (jqXHR) {
       console.log(jqXHR.status);
     }
   });
 
 });
 
-// 勾选尺码框事件
+// 勾选尺码复选框事件
 $('.size-checkboxs').on("change", ".size-checkbox", function (e) {
-  var submitData = JSON.stringify(formatSubmitData());
-  $('.table-responsive').attr('data-submitData', submitData);
 
+  var formatSubmitList = formatSubmitData();
+  // 获取勾选的尺码
+  var checkSizeDom = $('.size-checkboxs input:checked');
+  var checkSizeList = $.map(checkSizeDom, item => $(item).attr('data-cm'));
+  $.map(formatSubmitList, item => {
+    var cList = item.cList;
+    item.cList = $.map(checkSizeList, (cItem, index) => {
+      return { px: index, tm: '', cm: cItem, value: 0 }
+    })
+  })
+
+  console.log(formatSubmitList, 'formatSubmitList');
+  renderTable(formatSubmitList);
 });
+
 // 获取表格数据
 function fetchTableData(id) {
   $.ajax({
     type: "GET",
-    url: '/api/templateMaterial//getId/'+id,
+    url: '/api/templateMaterial//getId/' + id,
     dataType: "json",
     success: function (data) {
       renderTable(data);
@@ -359,9 +375,9 @@ function fetchTableData(id) {
       var data = {
         "code": "OK",
         "desc": "OK",
-        "item": [{
+        "item": [ {
           "metering_type": "吃2",
-          "cList": [{
+          "cList": [ {
             "px": 0,
             "tm": 2,
             "cm": "XS"
@@ -385,14 +401,14 @@ function fetchTableData(id) {
             "px": 5,
             "tm": 0,
             "cm": "XXL"
-          }],
+          } ],
           "part": "吃2",
           "size_base": "M",
           "error": 3,
           "size_base_value": null
         }, {
           "metering_type": "吃2",
-          "cList": [{
+          "cList": [ {
             "px": 0,
             "tm": 2,
             "cm": "XS"
@@ -416,14 +432,14 @@ function fetchTableData(id) {
             "px": 5,
             "tm": 0,
             "cm": "XXL"
-          }],
+          } ],
           "part": "吃2",
           "size_base": "M",
           "error": 3,
           "size_base_value": null
         }, {
           "metering_type": "吃2",
-          "cList": [{
+          "cList": [ {
             "px": 0,
             "tm": 2,
             "cm": "XS"
@@ -447,17 +463,17 @@ function fetchTableData(id) {
             "px": 5,
             "tm": 0,
             "cm": "XXL"
-          }],
+          } ],
           "part": "吃2",
           "size_base": "M",
           "error": 3,
           "size_base_value": null
-        }],
+        } ],
         "errParam": null,
         "failed": false,
         "success": true
       }
-      renderTable(data);
+      renderTable(data.item);
       renderSizecheckboxs(data);
     }
   });
